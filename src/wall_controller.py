6c371ec466759c40aca3f7d40842da6042cef27c
usi <user.name>
2020-04-02 18:55:15 +0200

    add support for rotated walls

--- src/wall_controller.py
+++ src/wall_controller.py
@@ -2,7 +2,7 @@ import rospy
 import movement_utils as mv
 from pid import PID
 from geometry_msgs.msg import Twist, Pose, Point
-from math import pi
+from math import pi, cos, sin
 from collections import deque
 
 
@@ -11,20 +11,19 @@ class WallController:
 		self.proximity_min_range = 0.010
 		self.proximity_max_range = 0.12
 		self.WALL_REACHED = False
-		self.DONE = False
+		self.PERPENDICULAR = False
 		self.ROTATING = False
 		self.ROTATED = False
 		self.MAX_DIST = False
-
-
+		self.DONE = False
 
 		self.proximity_threshold = proximity_threshold
 		self.motion_controller = mv.ToTargetPController(linear_speed=0.20,orientation_speed = 2.5)
 		self.debug = debug
-		self.target_orientation = pi
+		self.target_orientation = pi/2.
 
 
-		#self.theta_pid = PID(Kp=.2,Ki= 0.,Kd = 0.1)
+		self.theta_pid = PID(Kp=.2,Ki= 0.,Kd = 0.)
 		#self.error_buffer = deque(maxlen=8)
 
 
@@ -48,41 +47,32 @@ class WallController:
 				self.WALL_REACHED = True
 				print("Wall reached. Turning in place...")
 
-		if self.WALL_REACHED and not self.ROTATING:
-			self.ROTATING=True
-			self.target_orientation = (orientation+pi)%(2*pi)
+		if self.WALL_REACHED and not self.PERPENDICULAR:
+			max_orientation_speed = 0.75
+			angular_z = 20. * (proximity[4] - proximity[0])
+			module = min(abs(angular_z), max_orientation_speed)
+			angular_z *= module / abs(angular_z)
+			velocity.angular.z = angular_z
+
+			print(angular_z)
+			if abs(angular_z) < 0.01:
+				self.PERPENDICULAR=True
+				self.target_orientation = (orientation+pi)%(2*pi)
 
 
-		if not self.ROTATED and self.WALL_REACHED:
+		if not self.ROTATED and self.PERPENDICULAR:
 			if self.debug:
 				print("Rear proximity sensors values: lx->{0}---{1}<-rx".format(proximity[5],proximity[6]))
 				print("Orientation: {0}\n".format(orientation))
+
 			done , velocity = self.motion_controller.move(position,orientation,
 				position,target_orientation=self.target_orientation,
-				max_orientation_speed=.8
+				max_orientation_speed=.75
 			)
+
 			if done:
 				self.ROTATED = True
 				print("Done. Moving 2 meters away from the wall...")
-
-		"""
-		if not self.DONE and self.ROTATED:
-			
-			error = proximity[6]-proximity[5] 
-			self.error_buffer.append(error)
-			if len(self.error_buffer) == 8:
-				avg_error = sum(self.error_buffer)/8.
-				target = self.theta_pid.step(100* avg_error,dt=.1)
-				print("Average error: {0}; Correction: {1}".format(avg_error,target))
-				done , velocity = self.motion_controller.move(position,orientation,
-					position,target_orientation=-target+self.target_orientation,
-					max_orientation_speed=0.25
-				)
-				if done:
-					self.DONE = True
-			
-			velocity.linear.x = 0.
-			"""
 
 		if not self.MAX_DIST and self.ROTATED:
 			if proximity[5] < 0.1199:
@@ -91,7 +81,9 @@ class WallController:
 			else:
 				velocity.linear.x = 0.
 				self.final_target = Point()
-				self.final_target.x = position.x - (2. - 0.12 - 0.03)
+				distance = 2. - 0.12 - 0.03
+				self.final_target.y = position.y + sin(orientation) * distance
+				self.final_target.x = position.x + cos(orientation) * distance
 				self.MAX_DIST = True
 
 
