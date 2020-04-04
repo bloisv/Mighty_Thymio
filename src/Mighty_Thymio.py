0e0861dceeb3fc376007a826c6d33846b2a38201
Vinicius Bloise <bloisv@usi.ch>
2020-04-04 16:18:09 +0200

    minor format changes

--- src/Mighty_Thymio.py
+++ src/Mighty_Thymio.py
@@ -12,106 +12,100 @@ from explorer_controller import ExplorerController
 from math import pi
 
 
-
-
 class Mighty_Thymio:
-	def __init__(self, thymio_name):
-		# self.mode = 'FOLLOW8'
-		# self.mode = 'AVOID_WALL'
-		self.mode = 'EXPLORER'
-		rospy.init_node('Mighty_Thymio', anonymous=True)
-		self.rate = rospy.Rate(10)
+    def __init__(self, thymio_name):
+        # self.mode = 'FOLLOW8'
+        # self.mode = 'AVOID_WALL'
+        self.mode = 'EXPLORER'
+        rospy.init_node('Mighty_Thymio', anonymous=True)
+        self.rate = rospy.Rate(10)
 
-		
-		self.velocity_publisher = rospy.Publisher('/'+thymio_name+'/cmd_vel', Twist, queue_size=10)
-		self.odometry_subscriber = rospy.Subscriber('/'+thymio_name+'/odom', Odometry, self.update_pose)
-		
-		prx = '/'+thymio_name+'/proximity/'
-		self.proximity_sensors_name = [
-		 prx+'left',
-		 prx+'center_left', 
-		 prx+'center', 
-		 prx+'center_right',
-		 prx+'right',
-		 prx+'rear_left',
-		 prx+'rear_right'
-		]
+        self.velocity_publisher = rospy.Publisher('/' + thymio_name + '/cmd_vel', Twist, queue_size=10)
+        self.odometry_subscriber = rospy.Subscriber('/' + thymio_name + '/odom', Odometry, self.update_pose)
 
-		callback = [self.update_proximity_left,
-		self.update_proximity_center_left,
-		self.update_proximity_center,
-		self.update_proximity_center_right,
-		self.update_proximity_right,
-		self.update_proximity_rear_left,
-		self.update_proximity_rear_right
-		]
+        prx = '/' + thymio_name + '/proximity/'
+        self.proximity_sensors_name = [
+            prx + 'left',
+            prx + 'center_left',
+            prx + 'center',
+            prx + 'center_right',
+            prx + 'right',
+            prx + 'rear_left',
+            prx + 'rear_right'
+        ]
 
-		self.proximity_subscribers = [
-			rospy.Subscriber(sensor, Range, callback[i]) 
-			for i,sensor in enumerate(self.proximity_sensors_name)
-		]
+        callback = [self.update_proximity_left,
+                    self.update_proximity_center_left,
+                    self.update_proximity_center,
+                    self.update_proximity_center_right,
+                    self.update_proximity_right,
+                    self.update_proximity_rear_left,
+                    self.update_proximity_rear_right
+                    ]
 
-		self.position = Point()
-		self.orientation = 0
-		self.proximity = [0.12] * 7
-		self.covariance = None
+        self.proximity_subscribers = [
+            rospy.Subscriber(sensor, Range, callback[i])
+            for i, sensor in enumerate(self.proximity_sensors_name)
+        ]
 
+        self.position = Point()
+        self.orientation = 0
+        self.proximity = [0.12] * 7
+        self.covariance = None
 
-	def update_pose(self, data):
-		self.position = data.pose.pose.position
-		self.covariance = data.pose.covariance
-		quaternion = data.pose.pose.orientation
-		explicit_quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
-		_,_,self.orientation = euler_from_quaternion(explicit_quat)
-		self.orientation = mv.to_positive_angle(self.orientation)
+    def update_pose(self, data):
+        self.position = data.pose.pose.position
+        self.covariance = data.pose.covariance
+        quaternion = data.pose.pose.orientation
+        explicit_quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+        _, _, self.orientation = euler_from_quaternion(explicit_quat)
+        self.orientation = mv.to_positive_angle(self.orientation)
 
-	def update_proximity_left(self, data):
-		self.proximity[0] = data.range
+    def update_proximity_left(self, data):
+        self.proximity[0] = data.range
 
-	def update_proximity_center_left(self, data):
-		self.proximity[1] = data.range
-	
-	def update_proximity_center(self, data):
-		self.proximity[2] = data.range
+    def update_proximity_center_left(self, data):
+        self.proximity[1] = data.range
 
-	def update_proximity_center_right(self, data):
-		self.proximity[3] = data.range
+    def update_proximity_center(self, data):
+        self.proximity[2] = data.range
 
-	def update_proximity_right(self, data):
-		self.proximity[4] = data.range
+    def update_proximity_center_right(self, data):
+        self.proximity[3] = data.range
 
-	def update_proximity_rear_left(self, data):
-		self.proximity[5] = data.range
+    def update_proximity_right(self, data):
+        self.proximity[4] = data.range
 
-	def update_proximity_rear_right(self, data):
-		self.proximity[6] = data.range
-		
+    def update_proximity_rear_left(self, data):
+        self.proximity[5] = data.range
 
-	def run(self):
+    def update_proximity_rear_right(self, data):
+        self.proximity[6] = data.range
 
-		if self.mode == 'FOLLOW8':
-			self.controller = Follow8Controller()
-		elif self.mode == 'EXPLORER':
-			self.controller = ExplorerController()
-		else:
-			self.controller = WallController(0.2)
-		
+    def run(self):
 
-		while not rospy.is_shutdown():
-			if self.mode == 'FOLLOW8':
-				vel = self.controller.run(self.orientation) 
-			elif self.mode == 'EXPLORER':
-				vel = self.controller.run(self.proximity, self.position, self.orientation)
-			else:
-				vel = self.controller.run(self.proximity, self.position, self.orientation)
-			
-			self.velocity_publisher.publish(vel)
-			self.rate.sleep()
+        if self.mode == 'FOLLOW8':
+            self.controller = Follow8Controller()
+        elif self.mode == 'EXPLORER':
+            self.controller = ExplorerController()
+        else:
+            self.controller = WallController(0.2)
+
+        while not rospy.is_shutdown():
+            if self.mode == 'FOLLOW8':
+                vel = self.controller.run(self.orientation)
+            elif self.mode == 'EXPLORER':
+                vel = self.controller.run(self.proximity, self.position, self.orientation)
+            else:
+                vel = self.controller.run(self.proximity, self.position, self.orientation)
+
+            self.velocity_publisher.publish(vel)
+            self.rate.sleep()
 
 
 if __name__ == '__main__':
-	try:
-		Thymio_controller = Mighty_Thymio('thymio10')
-		Thymio_controller.run()
-	except rospy.ROSInterruptException as e:
-		pass
+    try:
+        Thymio_controller = Mighty_Thymio('thymio10')
+        Thymio_controller.run()
+    except rospy.ROSInterruptException as e:
+        pass
--- src/explorer_controller.py
+++ src/explorer_controller.py
@@ -6,81 +6,76 @@ from geometry_msgs.msg import Twist, Pose, Point
 from math import pi
 import random
 
+
 class ExplorerController:
-	
-	def __init__(self):
-		self.obstacle_controller = WallController(proximity_threshold=0.06, wall_safety_distance=.2, debug=False)
-		self.motion_controller = mv.ToTargetPController(linear_speed=0.20,orientation_speed = 2.5)
 
-		self.INIT = True
-		self.EXPLORING = False
-		self.OBSTACLE_DETECTED = False
-		self.OBSTACLE_AVOIDED = False
+    def __init__(self):
+        self.obstacle_controller = WallController(proximity_threshold=0.06, wall_safety_distance=.2, debug=False)
+        self.motion_controller = mv.ToTargetPController(linear_speed=0.20, orientation_speed=2.5)
 
+        self.INIT = True
+        self.EXPLORING = False
+        self.OBSTACLE_DETECTED = False
+        self.OBSTACLE_AVOIDED = False
 
-		self.velocity = Twist()
+        self.velocity = Twist()
 
-		rand_init_dir = random.randint(0,7)
-		self.init_orientation = pi/4 * rand_init_dir
+        rand_init_dir = random.randint(0, 7)
+        self.init_orientation = pi / 4 * rand_init_dir
 
-	def explore(self, proximity, position, orientation):
-		self.velocity.angular.z = 0.
-		
+    def explore(self, proximity, position, orientation):
+        self.velocity.angular.z = 0.
 
-		if self.obstacle_controller.is_obstacle_present(proximity):
-			self.EXPLORING = False
-			self.OBSTACLE_DETECTED = True
+        if self.obstacle_controller.is_obstacle_present(proximity):
+            self.EXPLORING = False
+            self.OBSTACLE_DETECTED = True
 
-			self.velocity.linear.x = 0.
-			return 
+            self.velocity.linear.x = 0.
+            return
 
-		self.velocity.linear.x = 0.10
+        self.velocity.linear.x = 0.10
 
+    def run(self, proximity, position, orientation):
 
+        if self.INIT:
+            done, vel = self.motion_controller.move(position, orientation,
+                                                    position, target_orientation=self.init_orientation,
+                                                    max_orientation_speed=.75
+                                                    )
+            self.velocity.linear.x = vel.linear.x
+            self.velocity.angular.z = vel.angular.z
 
-	def run(self, proximity, position, orientation):
+            if done:
+                self.INIT = False
+                self.EXPLORING = True
 
-		if self.INIT:
-			done , vel = self.motion_controller.move(position,orientation,
-				position,target_orientation=self.init_orientation,
-				max_orientation_speed=.75
-			)
-			self.velocity.linear.x = vel.linear.x
-			self.velocity.angular.z = vel.angular.z
+        if self.EXPLORING:
+            self.explore(proximity, position, orientation)
 
-			if done:
-				self.INIT=False
-				self.EXPLORING = True
+        if self.OBSTACLE_DETECTED:
+            vel = self.obstacle_controller.run(proximity, position, orientation)
+            self.velocity.linear.x = vel.linear.x
+            self.velocity.angular.z = vel.angular.z
 
+            if self.obstacle_controller.DONE:
+                self.OBSTACLE_AVOIDED = True
+                self.OBSTACLE_DETECTED = False
+                direction = 1 if random.random() < 0.5 else -1
+                self.new_orientation = mv.to_positive_angle(orientation + direction * pi / 2.)
 
-		if self.EXPLORING:
-			self.explore(proximity, position, orientation)
+        if self.OBSTACLE_AVOIDED:
+            # Select new direction randomly and start exploring again
 
+            done, vel = self.motion_controller.move(position, orientation,
+                                                    position, target_orientation=self.new_orientation,
+                                                    max_orientation_speed=.75
+                                                    )
+            self.velocity.linear.x = vel.linear.x
+            self.velocity.angular.z = vel.angular.z
 
-		if self.OBSTACLE_DETECTED:
-			vel = self.obstacle_controller.run(proximity, position, orientation)
-			self.velocity.linear.x = vel.linear.x
-			self.velocity.angular.z = vel.angular.z
+            if done:
+                self.EXPLORING = True
+                self.OBSTACLE_DETECTED = False
+                self.OBSTACLE_AVOIDED = False
 
-			if self.obstacle_controller.DONE:
-				self.OBSTACLE_AVOIDED = True
-				self.OBSTACLE_DETECTED = False
-				direction = 1 if random.random() < 0.5 else -1
-				self.new_orientation = mv.to_positive_angle(orientation + direction*pi/2.)
-
-		if self.OBSTACLE_AVOIDED:
-			# Select new direction randomly and start exploring again
-			
-			done , vel = self.motion_controller.move(position,orientation,
-				position,target_orientation=self.new_orientation,
-				max_orientation_speed=.75
-			)
-			self.velocity.linear.x = vel.linear.x
-			self.velocity.angular.z = vel.angular.z
-
-			if done:
-				self.EXPLORING = True
-				self.OBSTACLE_DETECTED = False
-				self.OBSTACLE_AVOIDED = False
-
-		return self.velocity
+        return self.velocity
